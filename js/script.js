132f5d7d3c9b3c6d0ddb3cc1511e7292fda931d7
NelliShevchenko <gho020507@gmail.com>
2024-09-14 21:07:56 +0300

    slidder js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,32 @@ menuIcon.addEventListener('click', () => {
 
     header.classList.toggle('header--mobile')
 });
+
+
+//slider wrapper//
+
+const sliderArrows = document.querySelector('.slider-arrows');
+slidesArrows = sliderArrows.querySelectorAll('.slider-arrows__item'),
+    prev = sliderArrows.querySelector('.slider-arrows__arrow--left'),
+    next = sliderArrows.querySelector('.slider-arrows__arrow--right');
+
+          
+console.log(next);
+
+let slideIndex = 0;
+
+prev.addEventListener('click', () => showSlideArrows(-1));
+next.addEventListener('click', () => showSlideArrows(1));
+
+function showSlideArrows(n = 0) {
+    slideIndex += n;
+
+    if (slideIndex < 0) slideIndex = slidesArrows.length - 1;
+    if (slideIndex >= slidesArrows.length) slideIndex = 0;
+    
+
+    sliderArrows.forEach(item => item.style.display = 'none');
+    slidesArrows[slideIndex].style.display = 'block';
+
+}
+showSlideArrows();
